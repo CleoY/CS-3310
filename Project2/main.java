98a22c77969d959dd171b173ed50c0e3b6d3ba8d
CleoY <77142560+CleoY@users.noreply.github.com>
2022-12-08 23:16:20 -0800

    An attempt at mm

--- Project2/main.java
+++ Project2/main.java
@@ -1,5 +1,6 @@
 package Project2;
 import java.util.ArrayList;
+import java.lang.Enum;
 
 public class main {
     public static void main(String args[]){
@@ -7,15 +8,18 @@ public class main {
         ArrayList<Integer> tempList = new ArrayList<Integer>();
 
         selection select = new selection();
-        randomList list1 = new randomList(10);
-        list1.printList();
-
+        randomList list2 = new randomList(10);
+        list2.printList();
+        int testK = 1;
         System.out.println("Recursive quick sort: ");
-        select.recursiveQuickSortSelect(list1.getList(), 10);
+        select.recursiveQuickSortSelect(list2.getList(), testK, 0);
         System.out.println("Iterative quick sort: ");
-        select.iterativeQuickSortSelect(list1.getList(), 10);
-        System.out.println("Merge sort: ");
-        select.mergeSortSelect(list1.getList(),1);
+        select.iterativeQuickSortSelect(list2.getList(), testK);
+        System.out.println("Merge sort: \n");
+        //select.mergeSortSelect(list2.getList(),1);
+        System.out.println("Median of medians sort: ");
+        select.mmSelect(list2.getList(), testK);
+        
 
         /*
         double mergeAvg = 0.0;
@@ -48,62 +52,76 @@ public class main {
 
         // k = 1, n/4, n/2, 3n/4, n
         // Can try enumerating k or something so I do not need to manually change k and run it many times? //////////
+        int n = 10;
         int k = 1;
-        int kth_smallest;
-
-        // Create 1000 different lists of size n = 10, 50, 100, 500, 100,...
-        // Test each list 20 times with the 4 different methods
-        for(int i=0; i<1000; i++){
-            //randomList list1 = new randomList(10);
-            for(int j=0; j<20; j++){
-                // Run merge sort algorithm
-                mergeUseCount++;
-                mergeStart = System.nanoTime();
-                select.mergeSort(list1, k);
-                // kth_smallest = select.mergeSort(list1, k); // for printing
-                mergeFinish = System.nanoTime();
-                mergeDuration = (mergeFinish - mergeStart) / 1000000;;
-                mergeTotalTime += mergeDuration;
-                mergeAvg = mergeTotalTime / mergeUseCount;
-
-                // Run iterative quick sort algorithm
-                iterQuickUseCount++;
-                iterQuickStart = System.nanoTime();
-                select.iterativeQuickSort(list1.getList(), k);
-                iterQuickFinish = System.nanoTime();
-                iterQuickDuration = (iterQuickFinish - iterQuickStart) / 1000000;;
-                iterQuickTotalTime += iterQuickDuration;
-                iterQuickAvg = iterQuickTotalTime / iterQuickUseCount;
-
-                // Run recursive quick sort algorithm
-                recurQuickUseCount++;
-                recurQuickStart = System.nanoTime();
-                select.recursiveQuickSort(list1.getList(), k);
-                recurQuickFinish = System.nanoTime();
-                recurQuickDuration = (recurQuickFinish - recurQuickStart) / 1000000;;
-                recurQuickTotalTime += recurQuickDuration;
-                recurQuickAvg = recurQuickTotalTime / recurQuickUseCount;
-
-                // Run recursive quick sort algorithm
-                mmUseCount++;
-                mmStart = System.nanoTime();
-                select.medianOfMedians(list1, k);
-                // kth_smallest = select.mmSort(list1, k); // for printing
-                mmFinish = System.nanoTime();
-                mmDuration = (mmFinish - mmStart) / 1000000;;
-                mmTotalTime += mmDuration;
-                mmAvg = mmTotalTime / mmUseCount;
+        int count = 1;
+        while(count<=5){
+            if(count == 2){
+                k = n/4;
+            } else if(count == 3){
+                k = n/2;
+            } else if(count == 4){
+                k = 3*n/4;
+            } else if(count == 5){
+                k = n;
             }
+            // Create 1000 different lists of size n = 10, 50, 100, 500, 100,...
+            // Test each list 20 times with the 4 different methods
+            for(int i=0; i<1000; i++){
+                randomList list1 = new randomList(n);
+                for(int j=0; j<20; j++){
+                    // Run merge sort algorithm
+                    mergeUseCount++;
+                    mergeStart = System.nanoTime();
+                    //select.mergeSort(list1, k);
+                    // kth_smallest = select.mergeSort(list1, k); // for printing
+                    mergeFinish = System.nanoTime();
+                    mergeDuration = (mergeFinish - mergeStart) / 1000000;;
+                    mergeTotalTime += mergeDuration;
+                    mergeAvg = mergeTotalTime / mergeUseCount;
+
+                    // Run iterative quick sort algorithm
+                    iterQuickUseCount++;
+                    iterQuickStart = System.nanoTime();
+                    select.iterativeQuickSortSelect(list1.getList(), k);
+                    iterQuickFinish = System.nanoTime();
+                    iterQuickDuration = (iterQuickFinish - iterQuickStart) / 1000000;;
+                    iterQuickTotalTime += iterQuickDuration;
+                    iterQuickAvg = iterQuickTotalTime / iterQuickUseCount;
+
+                    // Run recursive quick sort algorithm
+                    recurQuickUseCount++;
+                    recurQuickStart = System.nanoTime();
+                    select.recursiveQuickSortSelect(list1.getList(), k, 0);
+                    recurQuickFinish = System.nanoTime();
+                    recurQuickDuration = (recurQuickFinish - recurQuickStart) / 1000000;;
+                    recurQuickTotalTime += recurQuickDuration;
+                    recurQuickAvg = recurQuickTotalTime / recurQuickUseCount;
+
+                    // Run recursive quick sort algorithm
+                    mmUseCount++;
+                    mmStart = System.nanoTime();
+                    //select.medianOfMedians(list1, k);
+                    // kth_smallest = select.mmSort(list1, k); // for printing
+                    mmFinish = System.nanoTime();
+                    mmDuration = (mmFinish - mmStart) / 1000000;;
+                    mmTotalTime += mmDuration;
+                    mmAvg = mmTotalTime / mmUseCount;
+                }
+            }
+            System.out.println("k: "+k);
+            System.out.println("Merge sort avg time for 1000*20 runs: " + mergeAvg + " ms"); 
+            System.out.println("Iter quick sort avg time for 1000*20 runs: " + iterQuickAvg + " ms");
+            System.out.println("Recur quick sort avg time for 1000*20 runs: " + recurQuickAvg + " ms");
+            System.out.println("Median of medians avg time for 1000*20 runs: " + mmAvg + " ms");
+            System.out.println("");
+            count++;
         }
-        System.out.println("Merge sort average time for 1000*20 runs: " + mergeAvg + " ms"); 
-        System.out.println("Iterative quick sort method average time for 1000*20 runs: " + iterQuickAvg + " ms");
-        System.out.println("Recursive quick sort method average time for 1000*20 runs: " + recurQuickAvg + " ms");
-        System.out.println("Median of medians method average time for 1000*20 runs: " + mmAvg + " ms");
         
         double finish = System.nanoTime();
         double duration = finish - start;
         duration /= 1000000;
-        System.out.println("Duration: "+duration+" ms");
+        //System.out.println("Duration: "+duration+" ms");
         */
     }
 }
--- Project2/selection.java
+++ Project2/selection.java
@@ -8,47 +8,96 @@ public class selection {
 
     public void mergeSortSelect(ArrayList<Integer> givenList, int k){
         //mergeSort(givenList, 0, givenList.size()-1); // may need to 
-        ArrayList<Integer> temp = mergeSort(givenList, 0, givenList.size()-1);
-        printList(givenList, "Merge sort list: ");
+        // Make copy of givenList so temp list can be changed
+        ArrayList<Integer> temp = new ArrayList<Integer>(givenList.size());
+        for(int i=0; i<givenList.size(); i++){
+            temp.add(givenList.get(i));
+        }
+        //temp = mergeSort(temp, 0, givenList.size()-1);
+        mergeSort(temp, 0, givenList.size()-1);
+        printList(temp,"");
         selectKthSmallest(temp, k); 
     }
 
-    public ArrayList<Integer> mergeSort(ArrayList<Integer> givenList, int start, int end){
+    public void mergeSort(ArrayList<Integer> givenList, int start, int end){
         if(start<end){ //0<givenList.size()
             int mid = (start+end)/2;
             mergeSort(givenList, start, mid); // inf loop because givenList/givenList size does not change
             mergeSort(givenList, mid+1, end);
-            return merge(givenList, start, mid, end);
+            //givenList = merge(givenList, start, mid, end);
+            merge(givenList, start, mid, end);
         }
-        return null; // or given?
     }
 
-    public ArrayList<Integer> merge(ArrayList<Integer> givenList, int start, int mid, int end){
-        int i = start;
-        int j = mid + 1;
-        int k = start;
-        ArrayList<Integer> temp = new ArrayList<Integer>();
+    public void merge(ArrayList<Integer> givenList, int start, int mid, int end){
+        // int i = start;
+        // int j = mid + 1;
+        // int k = start;
 
-        while(i<=mid && j<=end){
-            if (givenList.get(i) < givenList.get(j)){
-                temp.add(givenList.get(i));
+        int i = start;
+        int j = mid;
+        int k = start;
+
+        ArrayList<Integer> left = new ArrayList<Integer>();
+        ArrayList<Integer> right = new ArrayList<Integer>();
+
+        for(int l=0; l<mid-start+1; l++){
+            left.add(givenList.get(start+l));
+        }
+        for(int r=0; r<end-mid; r++){
+            right.add(givenList.get(mid+1+r));
+        }
+
+        while(i<mid && j<end){
+            if(left.get(i) < right.get(j)){
+                givenList.set(k, left.get(i));
                 i++;
             } else{
-                temp.add(givenList.get(j));
+                givenList.set(k, right.get(j));
                 j++;
             }
             k++;
         }
+
         if(i>mid){
-            for(;j<end; j++){
-                temp.add(givenList.get(j));
+            for(;j<=end; j++){
+                givenList.set(k, right.get(j));
+                k++;
             }
         } else{
             for(;i<=mid; i++){
-                temp.add(givenList.get(i));
+                givenList.set(k, left.get(i));
+                k++;
             }
         }
-        return temp;
+
+
+
+    //     while(i<=mid && j<=end){
+    //         if (givenList.get(i) < givenList.get(j)){
+    //             temp.add(givenList.get(i));
+    //             i++;
+    //         } else{
+    //             temp.add(givenList.get(j));
+    //             j++;
+    //         }
+    //         k++;
+    //     }
+    //     if(i>mid){
+    //         for(;j<=end; j++){
+    //             temp.add(givenList.get(j));
+    //         }
+    //     } else{
+    //         for(;i<=mid; i++){
+    //             temp.add(givenList.get(i));
+    //         }
+    //     }
+    //     //printList(temp, "i is "+i);
+    //     // for(int l=0; l<givenList.size(); l++){
+    //     //     givenList.set(l, temp.get(l));
+    //     // }
+    //     givenList = temp;
+    //     //return temp;
     }
 
 
@@ -124,7 +173,7 @@ public class selection {
 
 
 
-    public void recursiveQuickSortSelect(ArrayList<Integer> givenList, int k){
+    public int recursiveQuickSortSelect(ArrayList<Integer> givenList, int k, int p){
         int pivot;
         int pivotIndex;
         ArrayList<Integer> smallerValues = new ArrayList<>(givenList.size()/2+1);
@@ -132,15 +181,31 @@ public class selection {
         
         // pivot = first element = kth smallest at the base case
         if(k==1 && givenList.size()==1){
-            System.out.println("Kth smallest element: " + givenList.get(0));
+            System.out.println("Kth smallest element: " + givenList.get(0) + "\n");
+            return givenList.get(0);
         } else{
-            pivot = givenList.get(0); // pivot = first element
+            int avoid;
+            if(p>-1){
+                //System.out.println("NOT -1");
+                pivot = givenList.get(p);
+                avoid = p;
+            } else{
+                //pivot = givenList.get(medianOfMedians(givenList));
+                //pivot = medianOfMedians(givenList);
+                avoid = medianOfMedians(givenList);
+                pivot = givenList.get(avoid);
+                System.out.println("MM pivot: "+pivot);
+            }
+            //pivot = givenList.get(0); // pivot = first element
             // sort values into list of smaller values and list of larger values
-            for(int i=1; i<givenList.size(); i++){
-                if(givenList.get(i) > pivot){
-                    largerValues.add(givenList.get(i));
-                } else{
-                    smallerValues.add(givenList.get(i));
+            
+            for(int i=0; i<givenList.size(); i++){
+                if(i!=avoid){
+                    if(givenList.get(i) > pivot){
+                        largerValues.add(givenList.get(i));
+                    } else{
+                        smallerValues.add(givenList.get(i));
+                    }
                 }
             }
             smallerValues.trimToSize();
@@ -150,23 +215,77 @@ public class selection {
             //System.out.println("K: "+k);
             pivotIndex = smallerValues.size() + 1;
             if(pivotIndex == k){
-                System.out.println("Kth smallest element: " + pivot);
+                System.out.println("Kth smallest element: " + pivot + "\n");
+                return pivot;
             } else if(k>smallerValues.size()){ // k is in list of larger values
                 //System.out.println("K-size-1: "+(k-smallerValues.size()-1));
                 //System.out.println("Focus on larger values");
-                recursiveQuickSortSelect(largerValues, k-smallerValues.size()-1);
+                recursiveQuickSortSelect(largerValues, k-smallerValues.size()-1, 0);
                 // scale k to a position relative to larger value list
             } else{ // k is in list of smaller values
                 //System.out.println("Focus on larger values");
-                recursiveQuickSortSelect(smallerValues, k);
+                recursiveQuickSortSelect(smallerValues, k, 0);
             }
         }
+        return -1;
     }
 
-
-
-    public void medianOfMedians(ArrayList<Integer> givenList, int k){
+    public void mmSelect(ArrayList<Integer> givenList, int k){
+        recursiveQuickSortSelect(givenList, k, -1);
+        //medianOfMedians(givenList);
         // selectKthSmallest();
+    }
+
+    public int medianOfMedians(ArrayList<Integer> givenList){
+        int n = givenList.size();
+        int subSize = (int) Math.sqrt(n);
+        System.out.println("subsize: "+subSize);
+        ArrayList <Integer> subList;
+        int subListMidIndex;
+        ArrayList <Integer> medians = new ArrayList<Integer>(subSize);
+        
+        for(int i=0; i<subSize; i++){
+            //System.out.println("Sublist i: "+i);
+            subList = new ArrayList<Integer>(subSize);
+            for(int j=i*subSize; j<i*subSize+subSize; j++){
+                //System.out.println("J: "+j);
+                //System.out.println("J+subsize: "+(i*subSize+subSize));
+                subList.add(givenList.get(j));
+            }
+
+            // In case there is excess in givenList, just add to last subList
+            if(i == subSize-1 && i*subSize+subSize<givenList.size()){
+                for(int j=i*subSize+subSize; j<givenList.size(); j++){
+                    //System.out.println("Extra index: "+j);
+                    subList.add(givenList.get(j));
+                }
+            }
+
+            printList(subList,"Sublist "+i);
+            subListMidIndex = (subList.size()+1)/2;
+            //System.out.println("Sublist median");
+            medians.add(findMedian(subList, subListMidIndex));
+        }
+        int mediansMidIndex = medians.size()/2;
+        printList(medians, "BIG medians");
+        int temp = findMedian(medians, mediansMidIndex); //actual mm but not INDEX of mm in givenList
+        System.out.println("Find BIG median return: "+temp);
+        int w=0;
+        for(; w<givenList.size(); w++){
+            if(givenList.get(w)==temp){
+                break;
+            }
+        }
+        return w;
+        //return -2;
+        //return findMedian(medians, mediansMidIndex);
+    }
+
+    public int findMedian(ArrayList<Integer> subList, int k){
+        int temp = recursiveQuickSortSelect(subList, k, 0);
+        System.out.println("SUBLIST median: "+temp);
+        return temp;
+        //return recursiveQuickSortSelect(subList, k, 0);
     }
 
 
